fd76ca7232b28d857ba4b3a95291ee759ffc4f36
wongwh2002 <wenghongwong5686@gmail.com>
2024-09-26 19:02:55 +0800

    A-Collections - even better!, refractored a bunch of stuff

--- src/main/java/Weng.java
+++ src/main/java/Weng.java
@@ -15,9 +15,84 @@ import static java.lang.Integer.parseInt;
 public class Weng {
     public static final String SEPARATOR = "____________________________________________________________";
     private static final ArrayList<Task> tasks = new ArrayList<>();
+    private static final String[] LEGAL_COMMANDS = {"bye", "list", "todo", "deadline", "event", "unmark", "mark", "delete"};
 
-    public static ArrayList<Task> getTasks() {
-        return tasks;
+    public static void main(String[] args) {
+        printGreeting();
+        inputVerification();
+    }
+
+    private static void inputVerification() {
+        Scanner scanner = new Scanner(System.in);
+        boolean isLooping = true;
+
+        while (isLooping) {
+            try {
+                String line = scanner.nextLine();
+                String[] currLine = line.split(" ");
+                printSeparator();
+
+                if (!isLegalCommand(currLine[0])) {
+                    throw new IllegalCommandException();
+                }
+
+                switch (currLine[0]) {
+                case "bye":
+                    printGoodbye();
+                    isLooping = false;
+                    break;
+                case "list":
+                    listTasks();
+                    break;
+                default:
+                    handleMultiWordCommands(currLine);
+                    break;
+                }
+            } catch (IndexOutOfBoundsException e) {
+                print("Index out of range");
+            } catch (IllegalCommandException e) {
+                print("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+            } catch (DescriptionEmptyException e) {
+                print("☹ OOPS!!! The description of a " + e.errorMessage + " cannot be empty.");
+            } catch (MissingDatesException e) {
+                print("☹ OOPS!!! The dates of a " + e.errorMessage + " cannot be empty.");
+            } finally {
+                printSeparator();
+            }
+        }
+    }
+
+    private static boolean isLegalCommand(String command) {
+        return Arrays.asList(LEGAL_COMMANDS).contains(command);
+    }
+
+    private static void handleMultiWordCommands(String[] currLine) throws DescriptionEmptyException, MissingDatesException, IllegalCommandException {
+        if (currLine.length < 2) {
+            throw new DescriptionEmptyException(currLine[0]);
+        }
+
+        switch (currLine[0]) {
+        case "todo":
+            addTodo(currLine);
+            break;
+        case "deadline":
+            addDeadline(currLine);
+            break;
+        case "event":
+            addEvent(currLine);
+            break;
+        case "unmark":
+            unmarkTask(currLine);
+            break;
+        case "mark":
+            markTask(currLine);
+            break;
+        case "delete":
+            deleteTask(currLine);
+            break;
+        default:
+            throw new IllegalCommandException();
+        }
     }
 
     public static void addTodo(String[] currLine) {
@@ -34,89 +109,15 @@ public class Weng {
     }
 
     public static void setIndexInTask(Task task, int index) {
-        Weng.tasks.set(index, task);
+        tasks.set(index, task);
     }
 
     public static int getTotalNumTasks() {
         return tasks.size();
     }
 
-    public static void main(String[] args) {
-        printGreeting();
-        inputVerification();
-    }
-
-    private static void inputVerification() {
-        Scanner scanner = new Scanner(System.in);
-        String line;
-        //LEGAL_COMMANDS only used in this method, so ill leave it here
-        String[] LEGAL_COMMANDS = {"bye", "list", "todo", "deadline", "event", "unmark", "mark", "delete"};
-        boolean isLooping = true;
-        while (isLooping) {
-            try {
-                //hmm, not sure how to refactor this code effectively
-                //do give comments if got any idea
-                line = scanner.nextLine();
-                String[] currLine = line.split(" ");
-                printSeparator();
-
-                if (Arrays.stream(LEGAL_COMMANDS).noneMatch(currLine[0]::equals)) {
-                    //if the command is not in the list of legal commands
-                    throw new IllegalCommandException();
-                }
-
-                switch (currLine[0]) {
-                case "bye":
-                    printGoodbye();
-                    isLooping = false; //break loop
-                    continue;
-                case "list":
-                    listTasks();
-                    continue;
-                }
-                //have done the 1-word commands, checking for multi-word commands now
-                if (currLine.length < 2) {
-                    throw new DescriptionEmptyException(currLine[0]);
-                }
-                switch (currLine[0]) {
-                case "todo":
-                    addTodo(currLine);
-                    break;
-                case "deadline":
-                    addDeadline(currLine);
-                    break;
-                case "event":
-                    addEvent(currLine);
-                    break;
-                case "unmark":
-                    unmarkTask(currLine);
-                    break;
-                case "mark":
-                    markTask(currLine);
-                    break;
-                case "delete":
-                    deleteTask(currLine);
-                    break;
-                default:
-                    throw new IllegalCommandException();
-                }
-            } catch (IndexOutOfBoundsException e) {
-                print("Index out of range");
-            } catch (IllegalCommandException e) {
-                print("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-            } catch (DescriptionEmptyException e) {
-                //probably need a separate exception for mark and unmark but ill do it next time
-                print("☹ OOPS!!! The description of a " + e.errorMessage + " cannot be empty.");
-            } catch (MissingDatesException e) {
-                print("☹ OOPS!!! The dates of a " + e.errorMessage + " cannot be empty.");
-            } finally {
-                printSeparator();
-            }
-        }
-    }
-
     private static void addAndPrintHandler(Task newTask) {
-        setIndexInTask(newTask, getTotalNumTasks());
+        tasks.add(newTask);
         print("Got it. I've added this task:");
         print("\t" + newTask.toString());
         print("Now you have " + getTotalNumTasks() + " tasks in the list.");
@@ -132,49 +133,45 @@ public class Weng {
 
     public static void deleteTask(String[] currLine) {
         int index = parseInt(currLine[1]);
-        if (getTotalNumTasks() < index || getTotalNumTasks() == 0) {
+        if (index >= getTotalNumTasks() || getTotalNumTasks() == 0) {
             throw new IndexOutOfBoundsException();
         }
         print("Noted. I've removed this task:");
-        print(getTasks().get(index).toString());
-        for (int i = index; i < getTotalNumTasks() - 1; i++) {
-            setIndexInTask(getTasks().get(i + 1), i);
-        }
+        print(tasks.get(index).toString());
+        tasks.remove(index);
         print("Now you have " + getTotalNumTasks() + " tasks in the list.");
     }
 
     public static void unmarkTask(String[] currLine) {
         int index = parseInt(currLine[1]);
-        if (getTotalNumTasks() < index) {
+        if (index >= getTotalNumTasks()) {
             print("Index out of range");
-        } else if (!getTasks().get(index).isDone()) {
+        } else if (!tasks.get(index).isDone()) {
             print("Already unmarked");
         } else {
+            tasks.get(index).setDone(false);
             print("Nice! I've marked this task as not done yet:");
-            getTasks().get(index).setDone(false);
+            print(tasks.get(index).toString());
         }
-        print(getTasks().get(index).toString());
     }
 
     public static void markTask(String[] currLine) {
         int index = parseInt(currLine[1]);
-        if (getTotalNumTasks() < index) {
+        if (index >= getTotalNumTasks()) {
             print("Index out of range");
+        } else if (tasks.get(index).isDone()) {
+            print("Already marked");
         } else {
-            if (getTasks().get(index).isDone()) {
-                print("Already marked");
-            } else {
-                print("Nice! I've marked this task as done:");
-                getTasks().get(index).setDone(true);
-            }
-            print(getTasks().get(index).toString());
+            tasks.get(index).setDone(true);
+            print("Nice! I've marked this task as done:");
+            print(tasks.get(index).toString());
         }
     }
 
     public static void listTasks() {
         print("Here are the tasks in your list:");
         for (int i = 0; i < getTotalNumTasks(); i++) {
-            print(String.format("%d. %s", i, getTasks().get(i)));
+            print(String.format("%d. %s", i, tasks.get(i)));
         }
     }
 
