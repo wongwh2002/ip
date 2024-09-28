4d0edee81f0e09ced22b08daa168d4ba81accff7
wongwh2002 <wenghongwong5686@gmail.com>
2024-09-28 11:00:40 +0800

    Removed and cleaned unused code

--- src/main/java/Tasks/Deadline.java
+++ src/main/java/Tasks/Deadline.java
@@ -1,10 +1,7 @@
 package Tasks;
 
-import Exceptions.MissingDatesException;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
 
 public class Deadline extends Task {
     public static final String BY = "/by";
@@ -19,17 +16,6 @@ public class Deadline extends Task {
         super(description, isDone);
         this.byDate = byDate;
     }
-
-//    public Deadline(String[] input) throws MissingDatesException {
-//        super(input[0]);
-//        int byIndex = Arrays.asList(input).indexOf(BY);
-//        if (byIndex == -1) {
-//            throw new MissingDatesException(BY);
-//        }
-//        setDescription(String.join(" ", Arrays.copyOfRange(input, 1, byIndex)));
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
-//        this.byDate = LocalDateTime.parse(String.join(" ", Arrays.copyOfRange(input, byIndex + 1, input.length)), formatter);
-//    }
 
     public LocalDateTime getByDate() {
         return byDate;
