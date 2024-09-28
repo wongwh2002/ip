5b6ee6b37b48830851d4959c93e484d62b8fd54c
wongwh2002 <wenghongwong5686@gmail.com>
2024-09-28 15:30:16 +0800

    moved to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "Zuke"
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
